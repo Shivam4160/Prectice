502d917ddd1712686c6f3f82696dd78889c45a14
Dhaval Solanki <dhavalas24@gmail.com>
2024-12-31 17:20:30 +0530

    31-12-2024

---
+++ Front-end/Javascript/loops/script.js
@@ -0,0 +1,86 @@
+//  loops
+
+// for in 
+
+{
+
+    let obj ={
+        name:"rahul",
+        height:"182cm",
+        age:19,
+        sport:"volleyball",
+        position:"ace",
+        year:"3rd"
+    }
+
+    console.log(obj.age);
+    console.log(obj.name);
+    
+    for(keys in obj){
+        console.log(`${keys} = ${obj[keys]}`);
+    }
+    
+
+    obj.score = 124;
+    console.log(obj.score);
+    
+    console.log("\n\n\n\n\n\n\n\n\n");
+}
+
+
+
+// for out
+
+{
+
+    let arr = [1,2,3,4,5,6,7,8,9,0]
+
+    for(i of arr){
+        console.log(`${i}`);
+    }
+
+
+    console.log("\n\n\n\n\n\n\n\n\n");
+
+}
+
+
+
+{
+
+    let rahul= {
+        name:"rahul",
+        height:"182cm",
+        age:19,
+        sport:"volleyball",
+        position:"ace",
+        year:"3rd"
+    }
+
+    let yash ={
+        name:"yash",
+        height:"161cm",
+        age:18,
+        sport:"volleyball",
+        position:"setter",
+        year:"2nd"
+    }
+
+    let set =new set(
+        
+    )
+
+    console.log(obj.age);
+    console.log(obj.name);
+    
+    for(keys in obj){
+        console.log(`${keys} = ${obj[keys]}`);
+    }
+    
+
+    obj.score = 124;
+    console.log(obj.score);
+    
+    console.log("\n\n\n\n\n\n\n\n\n");
+}
+
